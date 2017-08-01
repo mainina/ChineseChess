615227947b0dc1216623a0d242f0f8dbfd4d08dc
mainina <mainina200@sohu.com>
2017-08-01 14:38:24 +0800

    add resources

--- ChineseChess/ChineseChess/Resource.h
+++ ChineseChess/ChineseChess/Resource.h
@@ -1,31 +1,55 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by ChineseChess.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ChineseChess.rc 使用
 //
+#define IDC_MYICON                      2
+#define IDD_CHINESECHESS_DIALOG         102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_CHINESECHESS                109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     130
+#define IDB_BITMAP2                     131
+#define IDB_BITMAP3                     132
+#define IDB_BITMAP4                     133
+#define IDB_BITMAP5                     134
+#define IDB_BITMAP6                     135
+#define IDB_BITMAP7                     136
+#define IDB_BITMAP8                     137
+#define IDR_WAVE1                       138
+#define IDR_WAVE2                       139
+#define IDR_WAVE3                       140
+#define IDR_WAVE4                       141
+#define IDI_ICON1                       142
+#define IDR_WAVE5                       143
+#define IDR_WAVE6                       144
+#define IDR_WAVE7                       145
+#define IDR_WAVE8                       146
+#define IDR_WAVE9                       147
+#define IDR_WAVE10                      148
+#define IDB_BITMAP9                     149
+#define IDB_BITMAP10                    150
+#define IDB_BITMAP11                    151
+#define IDB_BITMAP12                    152
+#define IDB_BITMAP13                    153
+#define IDB_BITMAP14                    154
+#define IDB_BITMAP15                    155
+#define IDB_BITMAP16                    156
+#define IDI_ICON2                       157
+#define IDR_WAVE11                      158
+#define IDI_ICON3                       159
+#define IDC_STATIC                      -1
 
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_CHINESECHESS_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_CHINESECHESS			107
-#define IDI_SMALL				108
-#define IDC_CHINESECHESS			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// 新对象的下一组默认值
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        160
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
