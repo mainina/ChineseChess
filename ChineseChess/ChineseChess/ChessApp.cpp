742788eb5fc8a21b5f534691ada7a88c78f17694
mainina <mainina200@sohu.com>
2017-08-10 21:47:29 +0800

    add click

--- ChineseChess/ChineseChess/ChessApp.cpp
+++ ChineseChess/ChineseChess/ChessApp.cpp
@@ -25,5 +25,37 @@ void ChessApp::Startup(void)
 
 void ChessApp::DrawBoard(HDC hdc)
 {
-	cBoard->DrawBoard(hdc);
+	HDC hdcTmp = cBoard->DrawBoard(hdc);
+	if (IsSelected())
+	{
+		int xx = BOARD_EDGE + sqSelected.x * SQUARE_SIZE;
+		int yy = BOARD_EDGE + sqSelected.y * SQUARE_SIZE;
+		DrawTransBmp(hdc, hdcTmp, xx, yy, bmpSelected);
+	}
+	DeleteDC(hdcTmp);
+}
+
+// »æÖÆ¸ñ×Ó
+void ChessApp::DrawSquare(POINT pieceLocation, BOOL bSelected = FALSE) {
+	int xx, yy;
+
+	xx = BOARD_EDGE + pieceLocation.x * SQUARE_SIZE;
+	yy = BOARD_EDGE + pieceLocation.y * SQUARE_SIZE;
+	SelectObject(hdcTmp, cBoard->Bmp());
+	BitBlt(hdc, xx, yy, SQUARE_SIZE, SQUARE_SIZE, hdcTmp, xx, yy, SRCCOPY);
+
+	ChessPiece* piece = cBoard->FindPiece(pieceLocation.x, pieceLocation.y);
+	if (piece != NULL) {
+		DrawTransBmp(hdc, hdcTmp, xx, yy, piece->Bmp());
+	}
+	if (bSelected) {
+		DrawTransBmp(hdc, hdcTmp, xx, yy, bmpSelected);
+	}
+}
+
+void ChessApp::Click(int x, int y)
+{
+	/***
+
+	***/
 }
--- ChineseChess/ChineseChess/ChessApp.h
+++ ChineseChess/ChineseChess/ChessApp.h
@@ -10,11 +10,20 @@ private:
 public:
 	HINSTANCE hInst;
 	HWND hWnd;
+	HDC hdc, hdcTmp;
+	POINT sqSelected;
+	HBITMAP bmpSelected;
+	MoveStep*	mvLast;
+	BOOL bFlipped;
 
 public:
 	ChessApp();
 	~ChessApp();
 	void Startup(void);
 	void DrawBoard(HDC hdc);
+	void DrawSquare(POINT pieceLocation, BOOL bSelected);
+	void NotSelect(void);
+	void Click(int x, int y);
+	BOOL IsSelected(void);
 };
 
